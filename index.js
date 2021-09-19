8e103a0f1aca739d562000eff65fe313d795b9dc
Tyler Denman <tylerdenman200@gmail.com>
2021-09-19 20:00:17 +0100

    test bot status

--- index.js
+++ index.js
@@ -92,4 +92,15 @@ client.on('interactionCreate', async interaction => {
   }
 });
 
+bot.on('ready', () => {
+  bot.user.setStatus('available')
+  bot.user.setPresence({
+      game: {
+          name: 'with depression',
+          type: "STREAMING",
+          url: "https://www.twitch.tv/monstercat"
+      }
+  });
+});
+
 client.login(token);
