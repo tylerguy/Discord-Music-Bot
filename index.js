edaf08a5a74ac8816bfdf0f230e095a7a046dcb2
Tyler Denman <tylerdenman200@gmail.com>
2021-09-19 19:12:31 +0100

    error correction

--- index.js
+++ index.js
@@ -155,25 +155,27 @@ module.exports = {
       });
     }
 
-if (
-  interaction.guild.me.voice.channelId &&
-  interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
-) {
-  return void interaction.reply({
-    content: 'You are not in my voice channel!',
-    ephemeral: true,
-  });
-}
+    if (
+      interaction.guild.me.voice.channelId &&
+      interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
+    ) {
+      return void interaction.reply({
+        content: 'You are not in my voice channel!',
+        ephemeral: true,
+      });
+    }
 
-await interaction.deferReply();
-const queue = player.getQueue(interaction.guildId);
-if (!queue || !queue.playing)
-  return void interaction.followUp({
-    content: '❌ | No music is being played!',
-  });
-const success = queue.setPaused(true);
-return void interaction.followUp({
-  content: success ? '⏸ | Paused!' : '❌ | Something went wrong!',
-});
+    await interaction.deferReply();
+    const queue = player.getQueue(interaction.guildId);
+    if (!queue || !queue.playing)
+      return void interaction.followUp({
+        content: '❌ | No music is being played!',
+      });
+    const success = queue.setPaused(true);
+    return void interaction.followUp({
+      content: success ? '⏸ | Paused!' : '❌ | Something went wrong!',
+    });
+  },
+};
 
 client.login(token);
