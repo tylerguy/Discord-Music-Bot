0ea462ac36cc4d9685391a1f7bbc958ff2dcb10f
Restyled.io <commits@restyled.io>
2021-09-20 19:38:00 +0000

    Restyled by prettier

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const fs = require('fs');
-const Discord = require('discord.js');
-const Client = require('./client/Client');
-const {token} = require('./config.json');
-const {Player} = require('discord-player');
+const fs = require("fs");
+const Discord = require("discord.js");
+const Client = require("./client/Client");
+const { token } = require("./config.json");
+const { Player } = require("discord-player");
 
 const client = new Client();
 client.commands = new Discord.Collection();
 
-const commandFiles =
-    fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -19,65 +20,79 @@ console.log(client.commands);
 
 const player = new Player(client);
 
-player.on('error', (queue, error) => {
+player.on("error", (queue, error) => {
   console.log(
-      `[${queue.guild.name}] Error emitted from the queue: ${error.message}`);
+    `[${queue.guild.name}] Error emitted from the queue: ${error.message}`
+  );
 });
 
-player.on('connectionError', (queue, error) => {
-  console.log(`[${queue.guild.name}] Error emitted from the connection: ${
-      error.message}`);
+player.on("connectionError", (queue, error) => {
+  console.log(
+    `[${queue.guild.name}] Error emitted from the connection: ${error.message}`
+  );
 });
 
-player.on('trackStart', (queue, track) => {
-  queue.metadata.send(`Started playing: **${track.title}** in **${
-      queue.connection.channel.name}**!`);
-});
-
-player.on(
-    'trackAdd',
-    (queue,
-     track) => { queue.metadata.send(`Track **${track.title}** queued!`); });
-
-player.on('botDisconnect', queue => {
+player.on("trackStart", (queue, track) => {
   queue.metadata.send(
-      'I was manually disconnected from the voice channel, clearing queue!');
+    `Started playing: **${track.title}** in **${queue.connection.channel.name}**!`
+  );
 });
 
-player.on('channelEmpty', queue => {
-  queue.metadata.send('Nobody is in the voice channel, leaving...');
+player.on("trackAdd", (queue, track) => {
+  queue.metadata.send(`Track **${track.title}** queued!`);
 });
 
-player.on('queueEnd', queue => { queue.metadata.send('Queue finished!'); });
+player.on("botDisconnect", (queue) => {
+  queue.metadata.send(
+    "I was manually disconnected from the voice channel, clearing queue!"
+  );
+});
 
-client.once('ready', async () => {
-  console.log('Ready!');
+player.on("channelEmpty", (queue) => {
+  queue.metadata.send("Nobody is in the voice channel, leaving...");
+});
+
+player.on("queueEnd", (queue) => {
+  queue.metadata.send("Queue finished!");
+});
+
+client.once("ready", async () => {
+  console.log("Ready!");
   client.user
-      .setActivity(client.guilds.size + " server" +
-                       (client.guilds.size === 1 ? "" : "s"),
-                   {type : "WATCHING"})
-      .catch(console.error);
+    .setActivity(
+      client.guilds.size + " server" + (client.guilds.size === 1 ? "" : "s"),
+      { type: "WATCHING" }
+    )
+    .catch(console.error);
 });
 
-client.once('reconnecting', () => { console.log('Reconnecting!'); });
+client.once("reconnecting", () => {
+  console.log("Reconnecting!");
+});
 
-client.once('disconnect', () => { console.log('Disconnect!'); });
+client.once("disconnect", () => {
+  console.log("Disconnect!");
+});
 
-client.on('messageCreate', async message => {
-  if (message.author.bot || !message.guild)
-    return;
-  if (!client.application?.owner)
-    await client.application?.fetch();
+client.on("messageCreate", async (message) => {
+  if (message.author.bot || !message.guild) return;
+  if (!client.application?.owner) await client.application?.fetch();
 
-  if (message.content === '!deploy' &&
-      message.author.id === client.application?.owner?.id) {
-    await message.guild.commands.set(client.commands)
-        .then(() => { message.reply('Deployed!'); })
-        .catch(err => {
-          message.reply(
-              'Could not deploy commands! Make sure the bot has the application.commands permission!');
-          console.error(err);
-        });
+  if (
+    message.content === "!deploy" &&
+    message.author.id === client.application?.owner?.id
+  ) {
+    await message.guild.commands
+      .set(client.commands)
+      .then(() => {
+        message.reply("Deployed!");
+      })
+      .catch((err) => {
+        message.reply(
+          "Could not deploy commands! Make sure the bot has the application.commands permission!"
+        );
+        console.error(err);
+      });
   }
 });
 
