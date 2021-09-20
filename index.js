81c382f699e917804015237f43759450a6cda1a5
Tyler Denman <tylerdenman200@gmail.com>
2021-09-20 20:36:25 +0100

    change status of bot

--- index.js
+++ index.js
@@ -48,6 +48,7 @@ player.on('queueEnd', queue => {
 
 client.once('ready', async () => {
   console.log('Ready!');
+  client.user.setActivity(client.guilds.size + " server" + (client.guilds.size === 1 ? "" : "s"), { type: "WATCHING" }).catch(console.error);
  
 });
 
@@ -74,23 +75,7 @@ client.on('messageCreate', async message => {
         console.error(err);
       });
   }
-});
 
-client.on('interactionCreate', async interaction => {
-  const command = client.commands.get(interaction.commandName.toLowerCase());
-
-  try {
-    if (interaction.commandName == 'ban' || interaction.commandName == 'userinfo') {
-      command.execute(interaction, client);
-    } else {
-      command.execute(interaction, player);
-    }
-  } catch (error) {
-    console.error(error);
-    interaction.followUp({
-      content: 'There was an error trying to execute that command!',
-    });
-  }
 });
 
 client.login(token);
