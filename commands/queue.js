c477a32359fc377a4388727b46df21e9908b17b0
Restyled.io <commits@restyled.io>
2021-09-21 11:55:28 +0000

    Restyled by clang-format

--- commands/queue.js
+++ commands/queue.js
@@ -1,30 +1,27 @@
-const { GuildMember } = require("discord.js");
+const {GuildMember} = require("discord.js");
 
 module.exports = {
-  name: "queue",
-  description: "View the queue of current songs!",
+  name : "queue",
+  description : "View the queue of current songs!",
 
   async execute(interaction, player) {
- 
-
-
-          
-          var queue = player.getQueue(interaction.guildId);
-          if (typeof(queue) != 'undefined') {
-            trimString = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
-              return void interaction.reply({
-                embeds: [
-                  {
-                    title: 'Now Playing',
-                    description: trimString(`The Current song playing is **${queue.current.title}**! \n \n **${queue}**! `, 4095),
-                  }
-                ]
-              })
-          } else {
-            return void interaction.reply({
-              content: 'There is no song in the queue!'
-            })
-          }
+    var queue = player.getQueue(interaction.guildId);
+    if (typeof (queue) != 'undefined') {
+      trimString = (str, max) =>
+          ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+      return void interaction.reply({
+        embeds : [ {
+          title : 'Now Playing',
+          description :
+              trimString(`The Current song playing is **${
+                             queue.current.title}**! \n \n **${queue}**! `,
+                         4095),
+        } ]
+      })
+    } else {
+      return void interaction.reply(
+          {content : 'There is no song in the queue!'})
     }
-  
+  }
+
 };
