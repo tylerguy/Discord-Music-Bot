add1a71fa50e9c43727b9f3fbff4cff4aa9d93b2
Restyled.io <commits@restyled.io>
2021-09-21 10:52:56 +0000

    Restyled by clang-format

--- commands/queue.js
+++ commands/queue.js
@@ -2,34 +2,34 @@ const {GuildMember} = require('discord.js');
 
 module.exports = {
 
-    name: 'queue',
-    description: 'View the queue of current songs!',
+  name : 'queue',
+  description : 'View the queue of current songs!',
 
-    async execute (interaction, player) {
-
-        if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
-            return void interaction.reply({
-              content: 'You are not in a voice channel!',
-              ephemeral: true,
-            });
-          }
-    
-          
-          var queue = player.getQueue(interaction.guildId);
-          if (typeof(queue) != 'undefined') {
-            trimString = (str, max) => ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
-              return void interaction.reply({
-                embeds: [
-                  {
-                    title: 'Now Playing',
-                    description: trimString(`The Current song playing is **${queue.current.title}**! \n \n **${queue}**! `, 4095),
-                  }
-                ]
-              })
-          } else {
-            return void interaction.reply({
-              content: 'There is no song in the queue!'
-            })
-          }
+  async execute(interaction, player) {
+    if (!(interaction.member instanceof GuildMember) ||
+        !interaction.member.voice.channel) {
+      return void interaction.reply({
+        content : 'You are not in a voice channel!',
+        ephemeral : true,
+      });
     }
+
+    var queue = player.getQueue(interaction.guildId);
+    if (typeof (queue) != 'undefined') {
+      trimString = (str, max) =>
+          ((str.length > max) ? `${str.slice(0, max - 3)}...` : str);
+      return void interaction.reply({
+        embeds : [ {
+          title : 'Now Playing',
+          description :
+              trimString(`The Current song playing is **${
+                             queue.current.title}**! \n \n **${queue}**! `,
+                         4095),
+        } ]
+      })
+    } else {
+      return void interaction.reply(
+          {content : 'There is no song in the queue!'})
+    }
+  }
 }
