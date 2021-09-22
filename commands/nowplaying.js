9925f7b8195a18533b93117b82def7932fc11511
Restyled.io <commits@restyled.io>
2021-09-22 15:51:50 +0000

    Restyled by clang-format

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,26 +1,23 @@
 const {GuildMember} = require('discord.js');
 
 module.exports = {
-  name: 'nowplaying',
-  description: 'Get the song that is currently playing.',
+  name : 'nowplaying',
+  description : 'Get the song that is currently playing.',
   async execute(interaction, player) {
-    
     await interaction.deferReply();
     const queue = player.getQueue(interaction.guildId);
     if (!queue || !queue.playing)
       return void interaction.followUp({
-        content: 'No music is being played!',
+        content : 'No music is being played!',
       });
     const progress = queue.createProgressBar();
     const perc = queue.getPlayerTimestamp();
 
     return void interaction.followUp({
-      embeds: [
-        {
-          title: 'Now Playing',
-          description: `**${queue.current.title}**!`,
-        }
-        ],
+      embeds : [ {
+        title : 'Now Playing',
+        description : `**${queue.current.title}**!`,
+      } ],
     });
   },
 };
