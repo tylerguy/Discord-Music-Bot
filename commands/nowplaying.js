b211959bf5aea1a57d43ab85ac664877a02bef76
Tyler Denman <tylerdenman200@gmail.com>
2021-09-22 16:42:01 +0100

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -4,23 +4,7 @@ module.exports = {
   name: 'nowplaying',
   description: 'Get the song that is currently playing.',
   async execute(interaction, player) {
-    if (!(interaction.member instanceof GuildMember) || !interaction.member.voice.channel) {
-      return void interaction.reply({
-        content: 'You are not in a voice channel!',
-        ephemeral: true,
-      });
-    }
-
-    if (
-      interaction.guild.me.voice.channelId &&
-      interaction.member.voice.channelId !== interaction.guild.me.voice.channelId
-    ) {
-      return void interaction.reply({
-        content: 'You are not in my voice channel!',
-        ephemeral: true,
-      });
-    }
-
+    
     await interaction.deferReply();
     const queue = player.getQueue(interaction.guildId);
     if (!queue || !queue.playing)
