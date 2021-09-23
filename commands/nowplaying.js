3ee85503d993b3f35cfa3c832c380b2d24eea637
Restyled.io <commits@restyled.io>
2021-09-23 10:41:43 +0000

    Restyled by prettier

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -1,23 +1,23 @@
-const {GuildMember} = require("discord.js");
+const { GuildMember } = require("discord.js");
 
 module.exports = {
-  name : "nowplaying",
-  description : "Get the song that is currently playing.",
+  name: "nowplaying",
+  description: "Get the song that is currently playing.",
   async execute(interaction, player) {
     await interaction.deferReply();
     const queue = player.getQueue(interaction.guildId);
     if (!queue || !queue.playing)
       return void interaction.followUp({
-        content : "No music is being played!",
+        content: "No music is being played!",
       });
     const progress = queue.createProgressBar();
     const perc = queue.getPlayerTimestamp();
 
     return void interaction.followUp({
-      embeds : [
+      embeds: [
         {
-          title : "Now Playing",
-          description : `**${queue.current.title}**!`,
+          title: "Now Playing",
+          description: `**${queue.current.title}**!`,
         },
       ],
     });
